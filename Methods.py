169b09499519af7a43fa2e9b472aa171118dc798
S428786 <S428786@shhac150M13.ocsb.local>
2022-05-09 12:35:53 -0400

    Methods

---
+++ Methods.py
@@ -0,0 +1,16 @@
+# Methods are blocks of code that are executed with a command
+# Methods can be called anything, except keywords
+# predifined Python methods
+# Methods are created at the top of the program in Python 
+# Use the keyword "def" before this method
+
+# Method definition 
+def foo():
+    # code to be excecuted when method is called
+    print("Hello World")
+    print("Cool, Collected, and Catastrophic")
+    print("insert random text here")
+
+# Begining of Python program
+print("Welcome to my program.")
+foo()
