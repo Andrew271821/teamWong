73870225141eda620b9abee03573ef21e6ac68ac
S428786 <S428786@shhac150M13.ocsb.local>
2022-05-09 12:30:50 -0400

    Lists

---
+++ List.py
@@ -0,0 +1,8 @@
+andrewList = []
+
+andrewList2 = [1, "foo", 3.3, 'x']
+
+print(andrewList) #print entire list
+print(andrewList2) #print entire list
+print(andrewList2[3]) #print 3rd element in list
+print(andrewList2[1:3]) #print a range of elements 
