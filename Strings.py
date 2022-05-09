94fb76c52b2b9ca113b63565e1bbc43d7b4de526
S428786 <S428786@shhac150M13.ocsb.local>
2022-05-09 12:37:01 -0400

    Strings

---
+++ Strings.py
@@ -0,0 +1,28 @@
+'''
+Strings are a datatype.
+Strings can be changed.
+Strings are written within single or double quotation marks.
+All user input are strings
+'''
+
+string1 = "This is a string."
+
+string2 = "This is also a string"
+
+#print(string1)
+#print(string1[6])
+
+print(string1[:5])
+print(string1[:7])
+print(string1[:])
+print(string1 * 2)
+print(string1 * 8)
+
+var1 = ord('c')
+print(var1)
+
+var2 = chr(113)
+print(var2)
+
+#var1 = input("Please enter some text.")
+#print("You entered: " + var1)
