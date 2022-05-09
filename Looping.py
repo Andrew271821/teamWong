490bfb01900287934eeee25fe9e96582f6186669
S428786 <S428786@shhac150M13.ocsb.local>
2022-05-09 12:33:08 -0400

    Looping

---
+++ Looping.py
@@ -0,0 +1,27 @@
+"""
+
+for counter in range(0,12,2): 
+    print(counter)
+    print("hello")
+    
+myName = "Andrew Crabbe"
+    
+for letter in myName:
+    print(letter) 
+    
+
+
+number = 1                      
+
+while number < 10:
+    print("Hello World")
+    number = number + 1
+    
+"""
+count = 0
+userInput = int(input("Please enter a number: "))
+
+while count < userInput:
+    print(count)
+    # count += 1
+    count = count + 1
